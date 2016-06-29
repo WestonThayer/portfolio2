0a02fee4f7ab26a613486800d34e23c88b961440
Weston Thayer <weston@cryclops.com>
2016-06-28 20:47:18 -0700

    Bug: navbar looked blurry on chrome when zoomed in
    Only happens when you're in the navbar--compact state. This was because
    we left it with a transform3d still applied, which fuzzes the text a bit
    (probably doesn't want to run nice text anti-aliasing for perf reasons).
    
    Fix is to use keyframe animations and set top when the animation is
    done.

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -1,15 +1,58 @@
 (function() {
     document.addEventListener("DOMContentLoaded", function() {
         // Handle making the navbar sticky on desktop
-        var globalNavbar = document.getElementById("global-navbar");
-        
-        window.addEventListener("scroll", function(e) {
-            if (window.pageYOffset > 22) { // NOTE: This number must match the offset in _styles/partials/_navbar.scss
-                globalNavbar.classList.add("navbar--compact");
-            }
-            else {
-                globalNavbar.classList.remove("navbar--compact");
-            }
-        })
+        (function() {
+            var globalNavbar = document.getElementById("global-navbar");
+            var isPageAlreadyScrolled = true;
+            
+            window.addEventListener("scroll", function(e) {
+                // NOTE: This number must match $_vertDist in _styles/partials/_navbar.scss
+                var vertDist = 22;
+                
+                // Here's an explanation of what's happening in here in plain
+                // english since this is complex. There are a few possible flows:
+                //
+                // 1. User starts at the top of the page. No extra classes are supplied, navbar is in its vanilla state
+                // 2. User scrolls down past the threshold. We start the .navbar--compact--animation
+                // 3. When the animation completes, we add the .navbar--compact class. This uses `top` instead of `transform3d`, which results in clear text rendering for Chrome
+                // 4. User scrolls back up above threshold. We remove .navbar--compact and start the .navbar--expanded-animation
+                // 5. When that animation finishes, we simply remove it and go back to vanilla navbar
+                //
+                // 1. User starts in the middle of the page. We add .navbar--compact
+                if (window.pageYOffset > vertDist) {
+                    if (!globalNavbar.classList.contains("navbar--compact") &&
+                        !globalNavbar.classList.contains("navbar--compact-animation")) {
+                        globalNavbar.classList.remove("navbar--expanded-animation");
+                        
+                        if (isPageAlreadyScrolled) {
+                            globalNavbar.classList.add("navbar--compact");
+                        }
+                        else {
+                            globalNavbar.classList.add("navbar--compact-animation");
+                        }
+                    }
+                }
+                else {
+                    isPageAlreadyScrolled = false;
+                    
+                    if (globalNavbar.classList.contains("navbar--compact") ||
+                        globalNavbar.classList.contains("navbar--compact-animation")) {
+                        globalNavbar.classList.remove("navbar--compact");
+                        globalNavbar.classList.remove("navbar--compact-animation");
+                        globalNavbar.classList.add("navbar--expanded-animation");
+                    }
+                }
+            });
+            
+            globalNavbar.addEventListener("animationend", function(e) {
+                if (e.animationName === "navbar--compact-keyframes") {
+                    globalNavbar.classList.remove("navbar--compact-animation");
+                    globalNavbar.classList.add("navbar--compact");
+                }
+                else if (e.animationName === "navbar--expanded-keyframes") {
+                    globalNavbar.classList.remove("navbar--expanded-animation");
+                }
+            });
+        })();
     });
 })()
